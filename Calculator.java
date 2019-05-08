77420e3f086f29f891f322d2bb17fd867ea732c6
ppos78 <ppos78@gmail.com>
2019-05-08 21:35:53 +0900

    Add a minus function

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+package com.mycompany.test;
+
+public class Calculator {
+
+  public static void main(string[] args){
+
+  }
+  private static int sum(int a, int b){
+    return a+b;
+  }
+  private static int sum(int a, int b){
+    return a-b;
+  }
+}
